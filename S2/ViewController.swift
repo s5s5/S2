5ea85f7a5a4af0cce32a015f4e8d8ad616fac6a1
Super Liu <s5s5cn@gmail.com>
2015-05-26 16:48:19 +0800

    S2项目启动

---
+++ S2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S2
+//
+//  Created by s5s5 on 15/5/26.
+//  Copyright (c) 2015年 misuisui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
