d5eedb5b118bcfa9c8b8d735ecdaa7bb36248655
Super Liu <s5s5cn@gmail.com>
2015-06-04 16:46:40 +0800

    additem界面基本事件

--- S2/AddItemViewController.swift
+++ S2/AddItemViewController.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
 
   @IBAction func cancel() {
     dismissViewControllerAnimated(true, completion: nil)
   }
 
   @IBAction func done() {
+    println("Contents of the text field: \(textField.text)")
     dismissViewControllerAnimated(true, completion: nil)
   }
-  
+
   override func tableView(tableView: UITableView,
-    willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-      return nil
+                          willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+    return nil
+  }
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+
+  func textField(textField: UITextField,
+                 shouldChangeCharactersInRange range: NSRange,
+                 replacementString string: String) -> Bool {
+
+    let oldText: NSString = textField.text
+    let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+
+    doneBarButton.enabled = (newText.length > 0)
+
+    return true
   }
 
 }
